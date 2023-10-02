0710810a5d6b1b8293a9b4aebb940c680be2b80d
PatrisiusSatria <satriahendrawan51@gmail.com>
2023-10-02 17:15:53 +0700

    Tugas 1.2

---
+++ bin/tugas1.dart
@@ -0,0 +1,23 @@
+import 'package:tugas1/tugas1.dart' as tugas1;
+void main(List<String> arguments) {
+ Persegipanjang ukuran = Persegipanjang(5, 3);
+ int Keliling = ukuran.hitungKeliling() ;
+ int Luas = ukuran.hitungLuas();
+ print('Luas = $Luas cm ');
+ print('Keliling = $Keliling cm ');
+}
+
+
+class Persegipanjang {
+  int panjang;
+  int lebar;
+
+  Persegipanjang(this.panjang,this.lebar);
+
+  int hitungLuas() {
+    return panjang * lebar;
+  }
+  int  hitungKeliling() {
+    return 2 * (panjang + lebar );
+  }
+}
